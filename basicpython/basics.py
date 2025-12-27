8e515a71f51ebbf5aeb3361eb4609afa85ed3b41
Reddy Mani Simha <itsmanisimha@gmail.com>
2025-12-27 19:00:51 +0530

    basicpython

---
+++ basicpython/basics.py
@@ -0,0 +1,9 @@
+print("hello world")
+# ! basic function or defination in python
+def mani(n):
+    print(n)
+# here lets stand a question if we have reference of the function or defination can we use it in another file 
+# ? short answer Yes
+
+
+
